d7474c368170d8844dc320fa161d93bfc1ca2368
Kazzzemir <s1ckwell@mail.ru>
2023-05-31 14:43:25 +0300

    demo_v3

---
+++ js/index.js
@@ -0,0 +1,34 @@
+function makeAnimationWork(element) {
+    const elementStyles = getComputedStyle(element),
+        elementDisplay = getComputedStyle(element).display,
+        elementCoords = element.getBoundingClientRect().top * 1.5,
+        elementAnimationClass = element.dataset.animation
+        blankDiv = document.createElement('div')
+
+    blankDiv.style.height = elementStyles.height
+    blankDiv.style.width = elementStyles.width;
+    element.parentElement.replaceChild(blankDiv, element)
+
+    setTimeout(() => {
+        element.style.display = 'none'
+    })
+
+    addEventListenerForAnimations(element, blankDiv, elementDisplay, elementCoords, elementAnimationClass)
+}
+
+function addEventListenerForAnimations(element, blankDiv, elementDisplay, elementCoords, animationClass) {
+    window.addEventListener('scroll', () => {
+        if (window.scrollY + document.documentElement.clientHeight >= elementCoords) {
+            element.style.display = elementDisplay
+            element.classList.add(animationClass)
+            try {
+                blankDiv.parentElement.replaceChild(element, blankDiv)
+            } catch(e){}
+        }
+    })
+}
+
+
+document.querySelectorAll('.animate__animated').forEach(item => {
+    makeAnimationWork(item)
+})
